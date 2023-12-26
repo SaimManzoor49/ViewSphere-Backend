5fffba76c53d7dcd17498f4048de63730a09a533
unknown <saummanzoor49@gmail.com>
2023-12-26 21:22:40 +0500

    express+app-setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import * as dotenv from 'dotenv'
-import connectDB from './db/index.js';
+import * as dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log("app is listning on : " + (process.env.PORT || 8080));
+    });
+    app.on("error", () => {
+      console.log("Failed to connect app with DB");
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed", err);
+  });
