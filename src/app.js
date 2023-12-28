418a3668fdc543aadaa6e61910ac559d1647c78b
unknown <saummanzoor49@gmail.com>
2023-12-28 23:57:30 +0500

    user-register-route+controller

--- src/app.js
+++ src/app.js
@@ -1,31 +1,33 @@
-import express from 'express'
+import express from "express";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors(
-    {
-        origin:process.env.CORS_ORIGIN,
-        credentials:true,
-        
-    }
-))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use(express.json(
-    {
-        limit:'16kb'
-    }
-))
+// user router
+import userRouter from "./routes/user.routes.js";
 
-app.use(express.urlencoded({
-    extended:true,
-    limit:'16kb'
-}))
+app.use("/api/v1/users", userRouter);
+//
 
-app.use(express.static("public"))
-
-app.use(cookieParser())
-
-
-export {app}
+export { app };
